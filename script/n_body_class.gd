de74438bdf3e1346f81d71964fe41a085d9ed3d0
switt <switt@tu-clausthal.de>
2021-02-21 13:37:32 +0100

    ++

--- script/n_body_class.gd
+++ script/n_body_class.gd
@@ -15,7 +15,7 @@ var simulation_update_timer = 0
 
 var history = []
 export var history_lenth = 100
-var history_update_interfall = 0.03
+var history_update_interfall = 0.1
 var history_update_timer = 0
 
 # Called when the node enters the scene tree for the first time.
