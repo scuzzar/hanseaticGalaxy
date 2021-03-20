e6c57ad50c8756892052fc82654148365699dc16
switt <switt@tu-clausthal.de>
2021-03-20 07:50:11 +0100

    burn only if fuel added fuel cheat ,

--- Bodys/Ship.gd
+++ Bodys/Ship.gd
@@ -18,9 +18,11 @@ signal fuel_changed(fuel)
 func _integrate_forces(state:PhysicsDirectBodyState):
 	._integrate_forces(state)
 	$Model.trust_forward_off()
-	if Input.is_action_pressed("burn_forward"):	
-		$Model.trust_forward_on()
-		_burn_forward(state)
+	if Input.is_action_pressed("burn_forward"):
+		var fuelcost =  trust * state.step
+		if(fuel - fuelcost > 0):
+			$Model.trust_forward_on()
+			_burn_forward(state)
 	
 	_rotation(state,0)
 	if Input.is_action_pressed("trun_left"):
@@ -28,7 +30,9 @@ func _integrate_forces(state:PhysicsDirectBodyState):
 	
 	if Input.is_action_pressed("turn_right"):
 		_rotation(state,turn_rate*-1)
-		
+	
+	if Input.is_action_pressed("cheat_fuel"):
+		self.set_fuel(fuel_cap)
 	
 func _rotation(state :PhysicsDirectBodyState, angle: float):
 	state.set_angular_velocity(Vector3(0,angle,0))
@@ -39,9 +43,15 @@ func _get_forward_vector():
 	v = v.rotated(Vector3(0,1,0), orientation)
 	return v
 
-func _burn_forward(state:PhysicsDirectBodyState):
+func burn_fuel(fuel_cost:float):
+	self.set_fuel(fuel - fuel_cost)	
+	
+func set_fuel(pFuel:float):
+	fuel = pFuel
+	emit_signal("fuel_changed",fuel)
+
+func _burn_forward(state:PhysicsDirectBodyState):	
 	var force = _get_forward_vector()*trust
 	state.add_force(force, Vector3(0,0,0))
-	fuel = fuel - trust * state.step
-	emit_signal("fuel_changed",fuel)
+	self.burn_fuel(trust * state.step)
 	
--- Station/spacePort.gd
+++ Station/spacePort.gd
@@ -6,4 +6,5 @@ class_name Port
 func _on_Area_body_entered(body):
 	if(body is Ship):
 		var ship := body as Ship
-		ship.fuel = ship.fuel_cap
+		ship.set_fuel(ship.fuel_cap)
+		
