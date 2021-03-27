c18903d406060248041ef615a85861053ce2d48d
switt <switt@tu-clausthal.de>
2021-03-27 20:41:07 +0100

    HUD update Not integrated

--- HUD/HUD.gd
+++ HUD/HUD.gd
@@ -1,8 +1,9 @@
-extends MarginContainer
+extends Control
 
 var ship : Ship
-onready var fuel_label = $Bar/Fule_Box/Fuel/Value
-onready var fuel_bar = $Bar/FuelBar
+
+onready var fuel_bar = $Fuel/FuelBar
+onready var credit_labe = $Credits/HSplitContainer/Value
 func _enter_tree():
 	ship = $"../Ship"
 
@@ -15,5 +16,9 @@ func _process(delta):
 	pass
 
 func _on_Ship_fuel_changed(fuel):
-	fuel_label.text = String(round(fuel))
+	#fuel_label.text = String(round(fuel))
 	fuel_bar.value = int(fuel / ship.fuel_cap *100)
+
+
+func _on_Ship_credits_changed(credits):
+	credit_labe.text = str(credits)
