b1756a91f0458e605172144e787457948553de43
Stefan Wittek <switt@tu-clausthal.de>
2021-07-22 18:05:15 +0200

    Cleanup

--- scenes/Planet.gd
+++
@@ -1,8 +0,0 @@
-extends Rigid_N_Body
-
-export(Material) var material = preload("res://Bodys/Mars.material")
-
-func _ready():
-	$Mesh.material_override = material
-	self.simulation_delta_t = 10
-	._ready()
