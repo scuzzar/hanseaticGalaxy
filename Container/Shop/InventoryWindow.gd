cd76743c3213992d27767e282dc8312de55ea0c4
scuzzarGalaxy <scuzzarGalaxy@googlemail.com>
2021-11-13 18:12:44 +0100

    updated Port InventoryWindow

--- Container/Shop/InventoryWindow.gd
+++ Container/Shop/InventoryWindow.gd
@@ -1,4 +1,4 @@
-extends WindowDialog
+extends Panel
 
 
 # Declare member variables here. Examples:
@@ -8,9 +8,7 @@ extends WindowDialog
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.popup()
+	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func add_raw(good,destination,distance,reward):
+	pass
