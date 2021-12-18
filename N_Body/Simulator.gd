646e4e552593c82f574a84199045de31ebc8d52c
Stefan Wittek <switt@tu-clausthal.de>
2021-12-18 22:41:19 +0100

    impruved simulator

--- N_Body/Simulator.gd
+++ N_Body/Simulator.gd
@@ -1,8 +1,9 @@
 extends Node
 
 export var simulation_time = 100
+export var simulation_time_inc = 300
 export var simulation_delta_t = 1.0
-export var simulatoin_update_interfall = 1
+export var simulatoin_update_interfall = 1.0
 
 export(NodePath) var _simulation_object_path
 onready var _simulation_Object:Rigid_N_Body = get_node_or_null(_simulation_object_path)
@@ -10,10 +11,6 @@ onready var _simulation_Object:Rigid_N_Body = get_node_or_null(_simulation_objec
 export var on = false
 export var relativ_to_soi = false
 
-#export(NodePath) var _Target_object_path
-#onready var _target_Object:Rigid_N_Body = get_node_or_null(_simulation_object_path)
-
-#var simulation_orbit_treshold = 0.1
 
 var simulation_pos = []
 var simulation_vel = []
@@ -44,6 +41,18 @@ func toggel():
 		start()
 
 func _process(delta):
+	
+	if Input.is_action_just_pressed("simulate"):
+		self.toggel()
+	
+	if Input.is_action_just_pressed("simulate_more"):
+		if(on):
+			simulation_time +=simulation_time_inc
+	
+	if Input.is_action_just_pressed("simulate_less"):
+		if(on and (simulation_time -simulation_time_inc>0)):
+			simulation_time -=simulation_time_inc
+	
 	simulation_update_timer += delta	
 	if simulation_update_timer >= simulatoin_update_interfall:
 		simulation_update_timer -= simulatoin_update_interfall
@@ -62,19 +71,14 @@ func simulate():
 	
 	simulation_pos = [sim_obj_pos]
 	simulation_vel = [sim_obj_val]
-	var near_Planet = false
-	
-	var simSOI = null	
-	var simMaxG = 0
-		
-	#_simulation_Object.simulation_pos = []
+
 	var stronges_body_chang_count = 0
 	for i in simulation_steps:
 		var t = i * simulation_delta_t
 		var strongest_body:simpelPlanet = _simulation_Object.last_g_force_strongest_Body
 		var strongest_body_force :float = _simulation_Object.last_g_force_strongest_Body_force.length()
 		
-		if(stronges_body_chang_count>1): return
+		
 		var sim_g_force = Vector3(0,0,0)
 		for body in bodys:
 			var planet:simpelPlanet= body as simpelPlanet		
@@ -82,19 +86,18 @@ func simulate():
 				var other_translation = planet.predictGlobalPosition(t)
 				var sqrDst = sim_obj_pos.distance_squared_to(other_translation)
 				if(sqrDst <= planet.radius*planet.radius):return	
+				
 				var forcDir = sim_obj_pos.direction_to(other_translation).normalized()		
 				var acceleration = forcDir * _simulation_Object.G *planet.mass  / sqrDst
 				if(acceleration.length()>strongest_body_force):
 					strongest_body_force = acceleration.length()
 					strongest_body = planet
 					stronges_body_chang_count += 1
+					if(stronges_body_chang_count>1): return
 					$TargetPoint.translation = other_translation	
 					$TargetPoint.show()
 					$TargetPoint.scale = Vector3(1,1,1) * planet.radius	
 				sim_g_force += acceleration
-				var sqrRadius = planet.radius*planet.radius
-			
-					
 
 		sim_obj_val += sim_g_force   * simulation_delta_t /2
 		sim_obj_pos += sim_obj_val * simulation_delta_t
@@ -102,7 +105,3 @@ func simulate():
 		
 		simulation_pos.append(sim_obj_pos)
 
-
-		
-		#var new_relativ_point = sim_obj_pos-_simulation_Object.last_g_force_strongest_Body.global_transform.origin	
-		#_simulation_Object.simulation_pos.append(sim_obj_pos)
