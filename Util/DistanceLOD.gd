ef1ad5e0427a589ee21e399086631de27fdc69f5
scuzzarGalaxy <scuzzarGalaxy@googlemail.com>
2021-12-29 22:20:58 +0100

    Distance LOD for Ports

---
+++ Util/DistanceLOD.gd
@@ -0,0 +1,16 @@
+extends Node
+
+onready var cam = $"/root/Sol/Camera"
+
+export var active = true
+export var draw_distance = 10000
+var draw_distance_sqr = draw_distance*draw_distance
+
+func _process(delta):
+	var parent = get_parent()
+	var d_sqr = parent.global_transform.origin.distance_squared_to(cam.global_transform.origin)
+	if(d_sqr>draw_distance_sqr):
+		parent.hide()
+		pass
+	else:		
+		parent.show()
