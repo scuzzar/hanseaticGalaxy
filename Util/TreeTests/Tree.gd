9c565909491691ebcc50da1707f913effd8493ec
Scuzzar <scuzzarGalaxy@googlemail.com>
2022-10-02 11:12:16 +0200

    update of tree Test to include editable cells

--- Util/TreeTests/Tree.gd
+++ Util/TreeTests/Tree.gd
@@ -19,29 +19,54 @@ func _ready():
 	self.set_hide_root(true)
 	
 	for i in range(0,7):
-		_add_dummy_raw()
+		_add_dummy_raw(self)
 
 
 	
 
-func _add_dummy_raw():
+func _add_dummy_raw(meta):
 	var child1 = self.create_item(root)	
 
 	#child1.set_text_alignment(0,HORIZONTAL_ALIGNMENT_CENTER)
 	child1.set_cell_mode(0,TreeItem.CELL_MODE_CHECK)	
+	child1.set_editable(0,true)
 	
 	child1.set_text(1, "100")
-	child1.set_text_alignment(1,HORIZONTAL_ALIGNMENT_CENTER)
+	child1.set_text_alignment(1,HORIZONTAL_ALIGNMENT_CENTER)	
 	child1.set_text(2, "15653")
 	child1.set_text_alignment(2,HORIZONTAL_ALIGNMENT_CENTER)
 	child1.set_text(3, "15653")
 	child1.set_text_alignment(3,HORIZONTAL_ALIGNMENT_CENTER)
-	child1.set_text(4, "15653")
+	child1.set_text(4, "Fuuuuu")
+	child1.set_editable(4,true)
 	child1.set_text_alignment(4,HORIZONTAL_ALIGNMENT_CENTER)
-	child1.set_text(5, "15653")
+	
+	child1.set_cell_mode(5,TreeItem.CELL_MODE_RANGE)
+	child1.set_range_config(5,-500,500,10)
+	child1.set_range(5,0)
 	child1.set_text_alignment(5,HORIZONTAL_ALIGNMENT_CENTER)
-	child1
+	child1.set_editable(5,true)
+	
+		
+	
+	
+	child1.set_metadata(0,meta)
+	child1.set_selectable(0,false)
 	
 
 
+func _on_tree_item_selected():
+	var item = self.get_selected()
+	print(item)
+	var s = item.is_checked(0)
+	item.set_checked(0,not s)
+	
 
+
+func _on_tree_item_edited():
+	var item = self.get_edited()
+	var item_col = self.get_edited_column()
+	if(item_col == 0):
+		var meta = item.get_metadata(0)
+		print(meta.get_edited())
+	pass # Replace with function body.
