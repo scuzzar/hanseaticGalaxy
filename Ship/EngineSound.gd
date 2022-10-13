87de728b44671a36a27acd627f4245fef17e89e8
Scuzzar <scuzzarGalaxy@googlemail.com>
2022-10-13 17:02:12 +0200

    tuned truster down

--- Ship/EngineSound.gd
+++ Ship/EngineSound.gd
@@ -1,22 +1,23 @@
 extends AudioStreamPlayer3D
 
-@onready
-var tweenOut = get_tree().create_tween()
-
 @onready var tween : Tween
 
 @export var fadeIn_duration:float = 1
 @export var fadeOut_duration:float = 1
 
+var _db_target : float
+
 func _ready():
 	tween = get_tree().create_tween()	
+	_db_target = self.unit_db
+	unit_db = -80
 
 var transition_type = 1 # TRANS_SINE
 
 func play(from_position:float = 0.0):	
 	super.play(from_position)
 	tween = get_tree().create_tween()	
-	tween.tween_property(self, "unit_db", 0, fadeIn_duration)
+	tween.tween_property(self, "unit_db", _db_target, fadeIn_duration)
 
 func stop():	
 	tween = get_tree().create_tween()	
