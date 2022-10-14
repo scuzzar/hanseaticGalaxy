24caf92220ed773c1cbcc8b3d5c5eeed5a8e0e44
Scuzzar <scuzzarGalaxy@googlemail.com>
2022-10-14 20:00:18 +0200

    ++ for 4.0 beta 3

--- Ship/EngineSound.gd
+++ Ship/EngineSound.gd
@@ -9,8 +9,8 @@ var _db_target : float
 
 func _ready():
 	tween = get_tree().create_tween()	
-	_db_target = self.unit_db
-	unit_db = -80
+	_db_target = self.volume_db
+	self.volume_db = -80
 
 var transition_type = 1 # TRANS_SINE
 
@@ -18,11 +18,11 @@ func play(from_position:float = 0.0):
 	super.play(from_position)
 	tween.finished.disconnect(_on_TweenOut_tween_completed)
 	tween = get_tree().create_tween()	
-	tween.tween_property(self, "unit_db", _db_target, fadeIn_duration)
+	tween.tween_property(self, "volume_db", _db_target, fadeIn_duration)
 
 func stop():	
 	tween = get_tree().create_tween()	
-	tween.tween_property(self, "unit_db", -100, fadeOut_duration)
+	tween.tween_property(self, "volume_db", -100, fadeOut_duration)
 	tween.finished.connect(_on_TweenOut_tween_completed)
 
 func _on_TweenOut_tween_completed():
